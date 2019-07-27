f01a82ab56d7d3eadae396e1565595817df80cf7
dnjswp12 <49463724+dnjswp12@users.noreply.github.com>
2019-07-27 17:58:48 +0900

    Update Blink.c

--- GPIO/Blink.c
+++ GPIO/Blink.c
@@ -1,20 +1,10 @@
-/* Blink Example
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 
-/* Can run 'make menuconfig' to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
 //#define BLINK_GPIO  4// GPIO5>>blue led(sdkconfig.h), 34인풋만 가능한게 있다
 //0 2 4 5 12 13 14 15 18 19 21 22 23 25 26 27 32 33 34
 #define GPIO0	0

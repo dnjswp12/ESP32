5e339419a0a7752d255148180e0ab6d95d78fc1c
dnjswp12 <49463724+dnjswp12@users.noreply.github.com>
2019-07-27 16:59:27 +0900

    Update Blink.c

--- GPIO/Blink.c
+++ GPIO/Blink.c
@@ -47,14 +47,14 @@ void blink_task(void *pvParameter)
        functions.)
     */
     //gpio_pad_select_gpio(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
+    //Set the GPIO as a push/pull output 
     //gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
   /* while(1)
 		    {
-		            /* Blink off (output low) */
+		            // Blink off (output low)
 		            gpio_set_level(GPIO2, 0);
 		            vTaskDelay(100 / portTICK_PERIOD_MS);
-		            /* Blink on (output high) */
+		            //Blink on (output high) 
 		            gpio_set_level(GPIO2, 1);
 		            vTaskDelay(100 / portTICK_PERIOD_MS);
 		    }*/
